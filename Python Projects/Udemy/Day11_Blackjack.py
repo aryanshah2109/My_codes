51b0f9ba4d745013f50c9209ce56f0cb3cc5542a
aryan210905 <aryan2109shah@gmail.com>
2025-01-29 18:53:49 +0530

    29th Jan 2025

--- Python Projects/Udemy/Day11_Blackjack.py
+++ Python Projects/Udemy/Day11_Blackjack.py
@@ -1,73 +1,70 @@
 import random as r
 deck = [11,2,3,4,5,6,7,8,9,10,10,10,10]
-'''
-pickAgain = input("Do you want to pick another card? Type yes or no: ").lower()
-    if(pickAgain == "yes"):
-        pickAnotherCard(usersCards)
-    elif(pickAgain == "no"):
-        
-    else:
-        print("Invalid input! Exiting the game")
-        break
-'''
 
-def pickAnotherCard(usersCardsCurrent:list, dealersCardsCurrent:list):    
-    while True:
-        newCard = r.random(deck)
-        usersCardsCurrent.append(newCard)
-        print("Your cards are: ",usersCardsCurrent)
+def printUserCards(userCards:list):
+    print("Your cards are ",userCards)
+    userSum = sum(userCards)
+    print(f"Current sum is {userSum}")   
 
-        usersCardsSum = sum(usersCardsCurrent)
-        dealersCardsSum = sum(dealersCardsCurrent)
-        
-        print("Current sum: ",usersCardsSum)
-        if(dealersCardsSum > usersCardsSum and dealersCardsSum < 21):
-            print("The dealer won!")
-        elif((dealersCardsSum < usersCardsSum and usersCardsSum <= 21) or dealersCardsSum ):
-            print("You Won! Congratulations")
-        elif():
-            pass
-    
+def printDealersCards(dealersCards:list):
+    print("Dealers cards are ",dealersCards)
+    dealersSum = sum(dealersCards)
+    print(f"Current sum is {dealersSum}") 
 
+def checkConditions(userCards:list, dealersCards:list):
+    dealersSum = sum(dealersCards)
+    userSum = sum(userCards)
+    print(f"Dealers cards are {dealersCards}")
+    if( dealersSum > 21 ):
+        print("Your won!! The sum of dealers cards is over 21")
+        return
+    if(dealersSum < 21 and dealersSum > userSum):
+        print("You lost. The dealer is closer to 21")
+    elif(dealersSum < 21 and dealersSum < userSum and dealersSum > 13):
+        print("You won. You are closer to 21")
+    elif(dealersSum == userSum and dealersSum<=21 and userSum<=21):
+        print("Push! Its a tie")
+    elif(dealersSum == 21 and userSum != 21):
+        print("You lost. The sum of dealers cards is equal to 21")
 
-def checkReplay():
-    playAgain = input("Do you want to play again? Type yes or no: ").lower()
-    if(playAgain == "yes"):
-        return True
-    elif(playAgain == "no"):
-        return False
-    else:
-        print("Invalid input!")
-    
 print("Welcome to Blackjack!")
-while True:
-    usersCards = r.sample(deck,2)
-    dealersCards = r.sample(deck,2)
-    print("Your cards are ",usersCards)
-    print("Dealers' first card is: ",dealersCards[0])
-    initial_sum = sum(usersCards)
-    if(initial_sum == 21):
-        print("Blackjack!!!! You won. Congratulations")
-        if(checkReplay()):
-            continue
-        else:
+userCards = r.sample(deck,2)
+dealersCards = r.sample(deck,2)
+printUserCards(userCards)
+print("Dealers' first card is: ",dealersCards[0])
+
+if(sum(userCards) == 21):
+    print("Blackjack!!!! You won. Congratulations")
+    raise SystemExit
+while(True):
+    choice = input("Do you want to pick a card? Yes/No: ").lower()
+    if(choice=="yes"):      # hit condition
+        newCard = r.choice(deck)
+        userCards.append(newCard)
+        printUserCards(userCards)
+        if(sum(userCards)>21):
+            print("Bust! You lost. Your total went above 21")
             break
-    elif(initial_sum > 21):
-        print("Your initial total went over 21. You lost :( ")
-        if(checkReplay()):
-            continue
-        else:
+    elif(choice=="no"):     # halt condition
+        dealersSum = sum(dealersCards)
+        userSum = sum(userCards)
+        checkConditions(userCards,dealersCards)
+        while(dealersSum < 13):
+            print("Adding a new card to the dealers' cards....")
+            newCard = r.choice(deck)
+            dealersCards.append(newCard)
+            printDealersCards(dealersCards) 
+            if(dealersSum > 13):
+                checkConditions(userCards,dealersCards)
             break
-    while True:
-        pickAgain = input("Do you want to pick another card? Type yes or no: ").lower()
-        if(pickAgain == "yes"):
-            pickAnotherCard(usersCards)
-            
-        elif(pickAgain == "no"):
-            pass
-        else:
-            print("Invalid input! Exiting the game")
-            break
+        break
+    else:
+        print("Invalid input! Exiting the game!")
+        raise SystemExit
+        
+
+        
 
 
-    
+
+        
