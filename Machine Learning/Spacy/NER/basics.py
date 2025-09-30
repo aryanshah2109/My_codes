8cbfb96c1e11314dcea4f4226196b47d19d42a24
aryan210905 <aryan2109shah@gmail.com>
2025-09-30 18:51:48 +0530

    added fastapi and docker codes

--- Machine Learning/Spacy/NER/basics.py
+++
@@ -1,7 +0,0 @@
-import spacy
-nlp = spacy.load('en_core_web_sm')
-doc = nlp('Tesla is going to buy Twitter INC. for $45 billion')
-for ent in doc.ents:
-    print(ent.text,'|',ent.label_)
-
-print(doc.ents)
--- Machine Learning/Spacy/Tokenization/basic.py
+++
@@ -1,8 +0,0 @@
-import spacy
-nlp = spacy.load('en_core_web_sm')
-doc = nlp("Hey! My name is Aryan Shah. I am studying NLP while in my 4th semester. I pay $1600 as a fee in my college.")
-
-for sentences in doc.sents:
-    for words in sentences:
-        print(words)
-
